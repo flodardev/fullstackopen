fa10587c228d4853c7bfd55596350856b9c1bd98
flodardev <63950579+flodardev@users.noreply.github.com>
2026-07-31 13:38:09 +0800

    part 2 phonebook exercise step 7 and 8 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Numbers from "./components/Numbers"
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import numberService from "./services/numbers"
 import "./App.css"
 
 const App = () => {
@@ -14,11 +15,9 @@ const App = () => {
 
   // axios to db.json
   useEffect(() => {
-    axios
-    .get("http://localhost:3001/persons")
-    .then (response => {
-      setPersons(response.data)
-    })
+    numberService.getAll()
+      .then(numbers => setPersons(numbers))
+      .catch(error => alert("Problem getting data from server"))
   }, [])
 
   // handle form submission
@@ -30,21 +29,29 @@ const App = () => {
       persons.map(person => person.name.toLowerCase())
       .includes(newName.toLowerCase())
     ) {
+      // alert user name already exist
       alert(`${newName} already exists!`)
       setNewName("")
     } else {
       // push to the list
       const personObject = {
-        id: persons.length + 1,
+        id: String(persons.length + 1),
         name: newName,
-        number: Number(newNumber),
+        number: String(newNumber),
       }
 
-      setPersons(persons.concat(personObject))
+      // update db server
+      numberService.create(personObject)
+        .then(reponse => {
+          // update state
+          setPersons(persons.concat(personObject))
+        })
+        .catch(error => alert("error creating new person in db"))
 
       setNewName("")
       setNewNumber("")
     }
+
   }
 
   // handle name input
