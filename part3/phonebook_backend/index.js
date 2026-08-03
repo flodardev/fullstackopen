874ecc2d1c743447be3efb525e9876e565e16b0d
flodardev <63950579+flodardev@users.noreply.github.com>
2026-08-03 16:35:42 +0800

    init commit, 3.1: Phonebook backend step 1 done

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+
+const app = express();
+
+const persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// ROUTES
+app.get("/api/persons", (request, response) => {
+    if (persons) {
+        response.json(persons)
+    } else {
+        response.status(404).json({
+            status: 404,
+            error: "persons is missing from the server"
+        })
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
+})
